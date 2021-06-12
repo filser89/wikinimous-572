356a0de3695bb6dae115af19d46173c09bd6210d
Paul Desmond <pdesmond27jr@gmail.com>
2021-06-12 17:24:14 +0800

    added articles controller, views, seed file and coded index view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   has_many :reviews
   validates :title, presence: true
-  validates :title, length: { minimum: 6 }
+  validates :title, length: { minimum: 2 }
   validates :content, presence: true
 end
